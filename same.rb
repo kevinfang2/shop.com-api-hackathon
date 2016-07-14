a47b43fd0e891c7838863d900f1f23fcfa029649
myh1000 <myh1000@gmail.com>
2016-07-13 19:51:57 -0700

    wao

--- same.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require 'rest_client'
-require 'cgi'
-url = 'https://api.shop.com/AffiliatePublisherNetwork/v1/categories'
-headers  = { 'apikey' => 'l7xxa85a2511a8454491ac39f7a02cab7eb8' , :params => { CGI::escape('publisherID') => 'TEST',CGI::escape('locale') => 'en_US' } }
-response = RestClient::Request.execute :method => 'GET', :url => url , :headers => headers
-puts response
