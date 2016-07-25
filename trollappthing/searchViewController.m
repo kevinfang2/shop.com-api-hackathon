2cd733134346e681cfdb48340259e41623f54c07
myh1000 <myh1000@gmail.com>
2016-07-25 01:15:23 -0700

    image recog WIP

--- trollappthing/searchViewController.m
+++ trollappthing/searchViewController.m
@@ -7,16 +7,30 @@
 //
 
 #import "searchViewController.h"
+#import "ClarifaiClient.h"
+#import <CloudSight/CloudSight.h>
+
+static NSString * const kAppID = @"wsoRfJNqSNH67q1qWDgJHizF6jcX0elwolubIirz";
+static NSString * const kAppSecret = @"36f8k34jIGFuV7TXl0iktYh7d1IT6hz4FpbYj47G";
 
 @interface searchViewController (){
     cameraViewController *cameraView;
 }
 
 @property (weak, nonatomic) IBOutlet UITextField *searchField;
+@property (strong, nonatomic) ClarifaiClient *client;
 
 @end
 
 @implementation searchViewController
+- (ClarifaiClient *)client {
+    if (!_client) {
+        _client = [[ClarifaiClient alloc] initWithAppID:kAppID appSecret:kAppSecret];
+        // Uncomment this to request embeddings. Contact us to enable embeddings for your app:
+        // _client.enableEmbed = YES;
+    }
+    return _client;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -63,20 +77,61 @@
     [self presentViewController:imagePickerController animated:YES completion:NULL];
 }
 
-- (void)imagePickerController:(UIImagePickerController *)picker
-        didFinishPickingImage:(UIImage *)image
-                  editingInfo:(NSDictionary *)editingInfo
-{
-    // Dismiss the image selection, hide the picker and
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     
-    //show the image view with the picked image
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    NSData *imageData = UIImageJPEGRepresentation(chosenImage, 0.0);
+    NSString *imageString = [UIImageJPEGRepresentation(chosenImage, 0.0) base64EncodedStringWithOptions:0];
     
-    [picker dismissModalViewControllerAnimated:YES];
-    //UIImage *newImage = image;
+    [CloudSightConnection sharedInstance].consumerKey = @"w63eVgBk6UKS5zsK2ATaTA";
+    [CloudSightConnection sharedInstance].consumerSecret = @"EM8y1gD50g-PaBNVudqxuA";
+    
+    CloudSightQuery *query = [[CloudSightQuery alloc] initWithImage:imageData
+                                                         atLocation:CGPointMake(chosenImage.size.width/2, chosenImage.size.height/2)
+                                                       withDelegate:self
+                                                        atPlacemark: nil
+                                                       withDeviceId:[[[UIDevice currentDevice] identifierForVendor] UUIDString]];
+    
+    [query start];
+    
+    __block NSArray *tags;
+    [self.client recognizeJpegs:@[imageData] completion:^(NSArray *results, NSError *error) {
+        // Handle the response from Clarifai. This happens asynchronously.
+        if (error) {
+            NSLog(@"Error: %@", error);
+            NSLog(@"Sorry, there was an error recognizing the image.");
+        } else {
+            ClarifaiResult *result = results.firstObject;
+            
+            NSLog([NSString stringWithFormat:@"Tags:\n%@",
+                   [result.tags componentsJoinedByString:@", "]]);
+            NSString *string = [NSString stringWithFormat:@"%@", result.tags];
+            tags = [string componentsSeparatedByString: @","];
+            NSLog(@"the first tag is %@", tags[0]);
+            NSString *tag = tags[0];
+            tag = [tag substringWithRange:NSMakeRange(6, [tag length] - 6)];
+            NSLog(@"the modified tag is %@", tag);
+            
+            [self getRequest:(tag)];
+        }
+    }];
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
 }
 
 -(void)search:(NSString *)searchQuery {
     NSLog(@"%@",searchQuery);
+    [self getRequest:(searchQuery)];
+}
+
+-(void)getRequest:(NSString *)searchQuery {
     NSData *test = [cameraView getRequest:(searchQuery)];
     NSError *jsonError = nil;
     id jsonObject = [NSJSONSerialization JSONObjectWithData:test options:kNilOptions error:&jsonError];
@@ -114,26 +169,42 @@
             id jsonObject = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&jsonError];
             NSDictionary *jsonReq = (NSDictionary *)jsonObject;
             NSArray * values = [jsonReq objectForKey:@"products"];
-//            for (int x = 0; x<=10; x++){
-//                [_nameArray addObject:[values[x] objectForKey:@"name"]];
-//                [_priceArray addObject:[values[x] objectForKey:@"maximumPrice"]];
-//                [_linksArray addObject:[values[x] objectForKey:@"referralUrl"]];
-//                
-//                NSData * imageData = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString:[values[x] objectForKey:@"imageUrl"]]];
-//                UIImage * image = [UIImage imageWithData: imageData];
-//                [_imagesArray addObject:image];
-//            }
+            //            for (int x = 0; x<=10; x++){
+            //                [_nameArray addObject:[values[x] objectForKey:@"name"]];
+            //                [_priceArray addObject:[values[x] objectForKey:@"maximumPrice"]];
+            //                [_linksArray addObject:[values[x] objectForKey:@"referralUrl"]];
+            //
+            //                NSData * imageData = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString:[values[x] objectForKey:@"imageUrl"]]];
+            //                UIImage * image = [UIImage imageWithData: imageData];
+            //                [_imagesArray addObject:image];
+            //            }
         }
     }
-
 }
 
+- (void)cloudSightQueryDidFinishUploading:(CloudSightQuery *)query
+{
+    NSLog(@"uploaded");
+}
+
+- (void)cloudSightQueryDidFinishIdentifying:(CloudSightQuery *)query {
+    if (query.skipReason != nil) {
+        NSLog(@"Skipped: %@", query.skipReason);
+    } else {
+        NSLog(@"Identified: %@", query.title);
+    }
+}
+
+- (void)cloudSightQueryDidFail:(CloudSightQuery *)query withError:(NSError *)error {
+    NSLog(@"Error: %@", error);
+}
 -(void)dismissKeyboard {
     [_searchField resignFirstResponder];
 }
 
 
 -(void) unwindSegue{
+    [self dismissKeyboard];
     [self performSegueWithIdentifier: @"unwindSegue" sender:self];
 }
 
@@ -148,8 +219,8 @@
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+     Get the new view controller using [segue destinationViewController].
+     Pass the selected object to the new view controller.
 }
 */
 
